7ae8602ae0a2b22896b92ea3e79583e0393ae351
Jesse Bentley <jdev@Jesses-MacBook-Pro.local>
2019-08-08 12:33:04 -0400

    Contains Item - CB finished

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -64,7 +64,15 @@ console.log(multiplyNums(3, 6, multiply));
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for (let i = 0; i <= list.length; i++){
+    if (item === list[i]){
+      return cb = true;
+    }
+  }
 }
+
+const doesItExist = (doesIt) => console.log(doesIt);
+console.log('Gum', items, doesItExist)
 
 /* STRETCH PROBLEM */
 
